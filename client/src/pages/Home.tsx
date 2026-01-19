1693dd718d36cb02dcbcc8883484df010e0307f0
nickm538 <190541833+nickm538@users.noreply.github.com>
2026-01-19 13:25:07 -0500

    fix: Use correct variable name 'analysis' instead of 'analysisResult'

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1183,7 +1183,7 @@ export default function Home() {
                   {/* ML Prediction Results */}
                   <QuantMLAnalysis 
                     mlPrediction={mlPrediction} 
-                    r2Analysis={analysisResult?.advanced_technicals?.r2_analysis || analysisResult?.advanced_technicals}
+                    r2Analysis={analysis?.advanced_technicals?.r2_analysis || analysis?.advanced_technicals}
                     loading={mlPredictionMutation.isPending}
                   />
                 </div>
